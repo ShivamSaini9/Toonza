35766f73b331714858bc9f6796249701d2e4a629
Your NameShivam <shivamsaini839494@gmail.com>
2026-09-05 17:03:44 +0530

    fix backend server binding for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,16 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { connectRedis } from "./config/redis.js";
-import express from "express";
 import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
-// const app = express();
 
 connectRedis();
+
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 4000, "0.0.0.0", () => {
+      console.log(`Server is running on port ${process.env.PORT || 4000}`);
     });
   })
   .catch((error) => {
